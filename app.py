5a9c01399066c45e014ad15ba882f68010844d29
José Ramos <joseaprendeai@gmail.com>
2024-11-22 05:58:08 -0500

    Corrección para mantener datos si faltan campos y limpiar al finalizar

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def agregar_cliente():
     # Verificar que todos los campos obligatorios estén completos
     if not nombre_apellidos or not documento or not telefono:
         flash('Por favor, completa todos los campos obligatorios', 'danger')
-        return render_template('index.html', documento=documento, nombre_apellidos=nombre_apellidos, telefono=telefono, email=email)
+        return render_template('index.html', documento=documento, nombre_apellidos=nombre_apellidos, telefono=telefono, email=email, editar=False)
 
     # Verificar si ya existe un cliente con el mismo documento
     cliente_existente = Cliente.query.filter_by(documento=documento).first()
